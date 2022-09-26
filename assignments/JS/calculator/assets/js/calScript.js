e38a5e6ba8b6a4c5bb65dc3973f0ce01aba27d76
udyogi-siphara <udyogisiphara22@gmail.com>
2022-09-25 20:59:26 -0700

    Added clck funvtion for number buttons

---
+++ assignments/JS/calculator/assets/js/calScript.js
@@ -0,0 +1,39 @@
+$('#calNumber0').click(function (){
+   $('#calCurrentNumber').text("0");
+});
+
+$('#calNumber1').click(function (){
+    $('#calCurrentNumber').text("1");
+});
+
+$('#calNumber2').click(function (){
+    $('#calCurrentNumber').text("2");
+});
+
+$('#calNumber3').click(function (){
+    $('#calCurrentNumber').text("3");
+});
+
+$('#calNumber4').click(function (){
+    $('#calCurrentNumber').text("4");
+});
+
+$('#calNumber5').click(function (){
+    $('#calCurrentNumber').text("5");
+});
+
+$('#calNumber6').click(function (){
+    $('#calCurrentNumber').text("6");
+});
+
+$('#calNumber7').click(function (){
+    $('#calCurrentNumber').text("7");
+});
+
+$('#calNumber8').click(function (){
+    $('#calCurrentNumber').text("8");
+});
+
+$('#calNumber9').click(function (){
+    $('#calCurrentNumber').text("9");
+});
