55eb5084dd63b3c4c0792fca6b8339ee96ad1b8f
udyogi-siphara <udyogisiphara22@gmail.com>
2022-09-25 21:38:17 -0700

    Added Input numbers

--- assignments/JS/calculator/assets/js/calScript.js
+++ assignments/JS/calculator/assets/js/calScript.js
@@ -1,39 +1,114 @@
+let curNum = null;
+
 $('#calNumber0').click(function (){
    $('#calCurrentNumber').text("0");
+
+   if (!(curNum===null || curNum==="0")){
+       curNum += "0";
+       $('#calCurrentNumber').text(curNum);
+   }
+
 });
 
 $('#calNumber1').click(function (){
     $('#calCurrentNumber').text("1");
+
+    if (curNum===null){
+        curNum = "1";
+    }else{
+        curNum += "1";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber2').click(function (){
     $('#calCurrentNumber').text("2");
+
+    if (curNum===null){
+        curNum = "2";
+    }else{
+        curNum += "2";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber3').click(function (){
     $('#calCurrentNumber').text("3");
+
+    if (curNum===null){
+        curNum = "3";
+    }else{
+        curNum += "3";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber4').click(function (){
     $('#calCurrentNumber').text("4");
+
+    if (curNum===null){
+        curNum = "4";
+    }else{
+        curNum += "4";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber5').click(function (){
     $('#calCurrentNumber').text("5");
+
+    if (curNum===null){
+        curNum = "5";
+    }else{
+        curNum += "5";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber6').click(function (){
     $('#calCurrentNumber').text("6");
+
+    if (curNum===null){
+        curNum = "6";
+    }else{
+        curNum += "6";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber7').click(function (){
     $('#calCurrentNumber').text("7");
+
+    if (curNum===null){
+        curNum = "7";
+    }else{
+        curNum += "7";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber8').click(function (){
     $('#calCurrentNumber').text("8");
+
+    if (curNum===null){
+        curNum = "8";
+    }else{
+        curNum += "8";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
 
 $('#calNumber9').click(function (){
     $('#calCurrentNumber').text("9");
+
+    if (curNum===null){
+        curNum = "9";
+    }else{
+        curNum += "9";
+    }
+    $('#calCurrentNumber').text(curNum);
 });
+
+$('#calClear').click(function (){
+    $('#calCurrentNumber').remove();
+})
