f7fa01a7d8b46b4aab59fae902be2c2fa682d19f
udyogi-siphara <udyogisiphara22@gmail.com>
2022-09-28 22:12:14 -0700

    added Save customer and load customer

---
+++ assignments/CSS/frameworks/assets/js/customer.js
@@ -0,0 +1,44 @@
+let customerArray = [];
+
+$('#btnCusSave').click(function (){
+    let customerId = $('#txtCustomerId').val();
+    let customerName = $('#txtCustomerName').val();
+    let customerAddress = $('#txtCustomerAddress').val();
+    let customerSalary = $('#txtCustomerSalary').val();
+
+    var customer ={
+        id : customerId,
+        name : customerName,
+        address : customerAddress,
+        salary : customerSalary
+    }
+
+    customerArray.push(customer);
+    saveCustomer();
+    console.log(customerArray);
+});
+
+$('#btnSearchCustomer').click(function (){
+   let searchIdOrName = $('#searchIdOrName').includes();
+   for (searchIdOrName of customerArray[0]){
+        console.log(searchIdOrName.id);
+       $('#txtCusName').val(searchIdOrName.name);
+       console.log(searchIdOrName.name);
+       $('#txtCusAddress').val(searchIdOrName.address);
+       $('#txtCusSalary').val(searchIdOrName.salary);
+
+   }
+});
+
+
+
+
+
+
+/*Functions*/
+function saveCustomer(){
+    for (var i of customerArray){
+        $('#tblCustomer').append('<tr><td>'+i.id+'</td>'+'<td>'+i.name+'</td>'+'<td>'+i.address+'</td>'+'<td>'+i.salary+'</td></tr>');
+    }
+}
+
