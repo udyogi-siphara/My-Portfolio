5738372ab7550519282c99ef0dc7765d78ff5484
udyogi-siphara <udyogisiphara22@gmail.com>
2022-10-12 14:26:57 -0700

    Added regex for item

--- assignments/JS/SPA/assets/js/controller/itemController.js
+++ assignments/JS/SPA/assets/js/controller/itemController.js
@@ -113,12 +113,88 @@ $('#btnItemSave').on('keyup',function (){
     }
 });
 
+const itmCodeRegEx = /^(I00-)[0-9]{1,3}$/;
+const itmNameRegEx = /^[A-z ]{5,50}$/;
+const itmQtyRegEx = /^[0-9]{2,}$/;
+const itmPriceRegEx = /^[0-9]{1,}[.]?[0-9]{1,2}$/;
+
+let itemValidation = [];
+itemValidation.push({reg: itmCodeRegEx, field: $('#txtItemId'), error: 'Item Code Pattern is Wrong : I00-001'});
+itemValidation.push({reg: itmNameRegEx, field: $('#txtItemName'), error: 'Item Name Pattern is Wrong : A-z 5-50'});
+itemValidation.push({reg: itmQtyRegEx, field: $('#txtItemQty'), error: 'Item Quantity Pattern is Wrong : 0-9'});
+itemValidation.push({reg: itmPriceRegEx, field: $('#txtItemPrice'), error: 'Unit Price Pattern is Wrong : 100 or 100.00'});
+
+$('#txtItemId,#txtItemName,#txtItemQty,#txtItemPrice').on('keydown',function (){
+    if (event.key === 'Tab'){
+        event.preventDefault();
+    }
+});
+
+$('#txtItemId,#txtItemName,#txtItemQty,#txtItemPrice').on('keyup',function (){
+    checkValidation();
+});
+
+$('#txtItemId,#txtItemName,#txtItemQty,#txtItemPrice').on('blur',function (){
+    checkValidation();
+});
+
+$('#txtItemId').on('keydown',function (event){
+    if (event.key === 'Enter' && check(itmCodeRegEx,$('#txtItemId'))) {
+        $('#txtItemName').focus();
+    }
+});
+
+$('#txtItemName').on('keydown',function (event){
+    if (event.key === 'Enter' && check(itmNameRegEx,$('#txtItemName'))){
+        focusText($('#txtItemPrice'));
+    }
+});
+
+$('#txtItemPrice').on('keydown',function (event){
+    if (event.key === 'Enter' && check(itmPriceRegEx,$('#txtItemPrice'))){
+        focusText($('#txtItemQty'));
+    }
+});
+
+$("#txtItemQty").on('keydown', function (event) {
+    if (event.key == "Enter" && check(itmQtyRegEx, $("#txtItemQty"))) {
+        let response = confirm("Do you want to add this Item.?");
+        if (response) {
+            let itemCode = $('#txtItemId').val();
+            let itemName = $('#txtItemName').val();
+            let itemPrice = $('#txtItemPrice').val();
+            let itemQty =  $('#txtItemQty').val();
+
+            var item = {
+                code : itemCode,
+                name : itemName,
+                qty   : itemQty,
+                price : itemPrice
+            }
+
+            itemArray.push(item);
+            console.log(itemArray);
+            saveItem();
+            blindRowClickEvent();
+            clearAllText();
+
+        }
+    }
+});
+
+
+
+
+
+
+
 /*Functions*/
 function saveItem(){
     $("#tblItem> tr").detach();
     for (var i of itemArray){
         $('#tblItem').append('<tr><td>'+i.code+'</td>'+'<td>'+i.name+'</td>'+'<td>'+i.qty+'</td>'+'<td>'+i.price+'</td></tr>')
     }
+    blindRowClickEvent();
 }
 
 function clearItem(){
@@ -183,4 +259,65 @@ function setTextfieldValues(code,name,price,qty){
     $('#itemName').val(name);
     $('#itemPrice').val(price);
     $('#itemQty').val(qty);
+}
+
+/*Validations functions*/
+
+function checkValidation(){
+    let errorCount=0;
+    for (validation of itemValidation){
+        if (check(validation.reg, validation.field)){
+            setTextSuccess(validation.field,"");
+        }else{
+            errorCount=errorCount+1;
+            setTextError(validation.field,validation.error);
+        }
+    }
+    setButtonState(errorCount);
+}
+
+function check(regex,textField){
+    let inputValue = textField.val();
+    return regex.test(inputValue)? true : false ;
+}
+
+function defaultText(txtField,error) {
+    txtField.css("border", "1px solid #ced4da");
+    txtField.parent().children('span').text(error);
+}
+
+function setTextError(textField,error){
+    if (textField.val().length <= 0){
+        defaultText(textField,"");
+    }else{
+        textField.css("border", "1px solid red");
+        textField.parent().children('span').text(error);
+    }
+}
+
+function setTextSuccess(textField,error){
+    if (textField.val().length <= 0){
+        defaultText(textField,"");
+    }else{
+        textField.css("border", "1px solid green");
+        textField.parent().children('span').text(error);
+    }
+}
+
+function focusText(textField){
+    textField.focus();
+}
+
+function setButtonState(value){
+    if (value>0){
+        $('#btnItemSave').attr('disabled',true);
+    }else{
+        $('#btnItemSave').attr('disabled',false);
+    }
+}
+
+function clearAllText(){
+    $("#txtItemId").focus();
+    $('#txtItemId,#txtItemName,#txtItemQty,#txtCustomerPrice').val("");
+    checkValidation();
 }
