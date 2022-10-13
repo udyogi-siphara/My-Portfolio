14870e915b8c0a5789a7559b5eb9f5c6f9076beb
udyogi-siphara <udyogisiphara22@gmail.com>
2022-10-13 00:00:40 -0700

    select id and auto fill details

--- assignments/JS/SPA/controller/orderController.js
+++ assignments/JS/SPA/controller/orderController.js
@@ -11,3 +11,19 @@ function loadAllItemCode(){
         $('#itemCodeOrd').append(`<option>${itemCode.code}</option>`);
     }
 }
+
+$('#customerIdOrd').on('change',function (){
+   let customerId = searchCustomer($('#customerIdOrd').val());
+
+    $('#customerNameOrd').val(customerId.name);
+    $('#customerAddressOrd').val(customerId.address);
+    $('#customerSalaryOrd').val(customerId.salary);
+});
+
+$('#itemCodeOrd').on('change',function (){
+   let itemCode = searchItem($('#itemCodeOrd').val());
+
+    $('#itemNameOrd').val(itemCode.name);
+    $('#itemPriceOrd').val(itemCode.price);
+    $('#itemQtyOrd').val(itemCode.qty);
+});
