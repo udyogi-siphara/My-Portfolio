35bd25b6ff95d8f9357996e6aad68d25af063a2c
udyogi-siphara <udyogisiphara22@gmail.com>
2022-11-15 14:49:59 -0800

    Added breath and run boy

---
+++ assignments/JS/game/assets/js/gamejs.js
@@ -0,0 +1,46 @@
+var boy = document.getElementById('boy');
+let  idelImageNum = 0;
+let idelanimationNum = 0;
+
+function idelAnimation(){
+
+    idelImageNum = idelImageNum + 1;
+
+    if (idelImageNum === 11){
+        idelImageNum =1;
+    }
+
+    boy.src = "assets/image/Idle("+idelImageNum+").png";
+}
+
+
+function idelAnimationstart(){
+   idelanimationNum = setInterval(idelAnimation,200);
+}
+
+runImageNumder = 0;
+runAimatioNumber = 0;
+
+function runanimation(){
+    runImageNumder = runImageNumder + 1;
+
+    if (runImageNumder === 11){
+        runImageNumder = 1;
+    }
+
+    boy.src = "assets/image/Run("+runImageNumder+").png";
+
+}
+
+function runAnimationstart(){
+    runAimatioNumber = setInterval(runanimation,100);
+    clearInterval(idelanimationNum);
+}
+
+function keyChek(event){
+    if (event.key === 'Enter'){
+        if (runAimatioNumber === 0){
+            runAnimationstart();
+        }
+    }
+}
