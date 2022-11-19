8dce611146ccbc3ba17b28043ae57e0f7603f866
udyogi-siphara <udyogisiphara22@gmail.com>
2022-11-18 20:10:04 -0800

    Added jumping boy

--- assignments/JS/game/assets/js/gamejs.js
+++ assignments/JS/game/assets/js/gamejs.js
@@ -1,5 +1,5 @@
 var boy = document.getElementById('boy');
-let  idelImageNum = 0;
+let  idelImageNum = 1;
 let idelanimationNum = 0;
 
 /*BREATHING boy*/
@@ -19,7 +19,7 @@ function idelAnimationstart(){
    idelanimationNum = setInterval(idelAnimation,200);
 }
 
-runImageNumder = 0;
+runImageNumder = 1;
 runAimatioNumber = 0;
 
 function runanimation(){
@@ -39,15 +39,29 @@ function runAnimationstart(){
     clearInterval(idelanimationNum);
 }
 
+
+
 function keyChek(event){
+    console.log(event.key);
     if (event.key === 'Enter'){
         if (runAimatioNumber === 0){
             runAnimationstart();
         }
+        if (moveBackgrdAniId === 0){
+            moveBackgrdAniId = setInterval(moveBackground,100);
+        }
     }
 
-    if (moveBackgrdAniId === 0){
-        moveBackgrdAniId = setInterval(moveBackground,100);
+
+
+    if (event.key === 'ArrowUp'){
+        if (jumpAnimNum === 0){
+            jumpAnimationstart();
+        }
+        if (moveBackgrdAniId === 0){
+            moveBackgrdAniId = setInterval(moveBackground,100);
+        }
+
     }
 }
 
@@ -61,5 +75,46 @@ function moveBackground(){
 
     document.getElementById('background').style.backgroundPositionX = backgrounImagePostionX + "px";
 }
+
+/*JUMP boy*/
+
+let jumpAnimNum = 0;
+let jumpImageNum = 1;
+let boyMarginTop = 408;
+
+ function jumpAnimation(){
+     jumpImageNum = jumpImageNum + 1;
+
+     if(jumpImageNum <= 6){
+         boyMarginTop = boyMarginTop - 20;
+         boy.style.marginTop = boyMarginTop + "px";
+     }
+
+     if (jumpImageNum >= 7){
+         boyMarginTop = boyMarginTop + 20;
+         boy.style.marginTop = boyMarginTop + "px";
+     }
+
+     if (jumpImageNum === 11){
+         jumpImageNum = 1;
+         clearInterval(jumpAnimNum);
+         jumpAnimNum = 0;
+         runImageNumder=0;
+         runAnimationstart();
+     }
+
+     boy.src = "assets/image/Jump("+jumpImageNum+").png";
+ }
+
+ function jumpAnimationstart(){
+     clearInterval(idelanimationNum);
+     runImageNumder=0;
+     clearInterval(runAimatioNumber);
+     jumpAnimNum=setInterval(jumpAnimation,100);
+ }
+
+
+
+
 
 
