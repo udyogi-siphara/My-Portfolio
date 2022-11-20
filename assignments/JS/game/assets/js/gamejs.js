718f5d9bfcae495f2e163304013fbc8454a85e49
udyogi-siphara <udyogisiphara22@gmail.com>
2022-11-19 23:45:58 -0800

    Added level 4 with all of game

--- assignments/JS/game/assets/js/gamejs.js
+++ assignments/JS/game/assets/js/gamejs.js
@@ -3,6 +3,7 @@ let  idelImageNum = 1;
 let idelanimationNum = 0;
 
 /*BREATHING boy*/
+/*restartGame();*/
 function idelAnimation(){
 
     idelImageNum = idelImageNum + 1;
@@ -50,6 +51,11 @@ function keyChek(event){
         if (moveBackgrdAniId === 0){
             moveBackgrdAniId = setInterval(moveBackground,100);
         }
+
+        if (barrAnimationId === 0){
+            barrAnimationId = setInterval(barrAnimation,100);
+        }
+
     }
 
 
@@ -62,6 +68,10 @@ function keyChek(event){
             moveBackgrdAniId = setInterval(moveBackground,100);
         }
 
+        if (barrAnimationId === 0){
+            barrAnimationId = setInterval(barrAnimation,100);
+        }
+
     }
 }
 
@@ -69,29 +79,48 @@ function keyChek(event){
 let backgrounImagePostionX = 0;
 let moveBackgrdAniId = 0;
 
+/*SCORE line*/
+var score = 0;
+var level = 1;
+
+
 function moveBackground(){
 
     backgrounImagePostionX = backgrounImagePostionX - 20;
 
     document.getElementById('background').style.backgroundPositionX = backgrounImagePostionX + "px";
+
+    score = score +1;
+    document.getElementById('score').innerHTML = score;
+
+
+    level2();
+
+
+    /*if (score === 50){
+        var modal2 = document.getElementById('gameCompleteModal');
+        modal2.style.display = 'block';
+        level = level + 1 ;
+        document.getElementById('level').innerHTML = level;
+    }*/
 }
 
 /*JUMP boy*/
 
 let jumpAnimNum = 0;
 let jumpImageNum = 1;
-let boyMarginTop = 408;
+let boyMarginTop = 390;
 
  function jumpAnimation(){
      jumpImageNum = jumpImageNum + 1;
 
      if(jumpImageNum <= 6){
-         boyMarginTop = boyMarginTop - 20;
+         boyMarginTop = boyMarginTop - 55;
          boy.style.marginTop = boyMarginTop + "px";
      }
 
      if (jumpImageNum >= 7){
-         boyMarginTop = boyMarginTop + 20;
+         boyMarginTop = boyMarginTop + 55;
          boy.style.marginTop = boyMarginTop + "px";
      }
 
@@ -115,7 +144,7 @@ let boyMarginTop = 408;
 
  /*BARRIERS*/
 
-let barrMarginLeft = 500;
+let barrMarginLeft = 1540;
 
 function createBarriers(){
 
@@ -124,19 +153,125 @@ function createBarriers(){
         barr.className = 'barr';
         document.getElementById("background").appendChild(barr);
         barr.style.marginLeft = barrMarginLeft + "px";
+        barr.id = "barr"  + i;
 
         /*barrMarginLeft = barrMarginLeft + 500;*/
 
         if (i<5){
-            barrMarginLeft = barrMarginLeft + 500;
+            barrMarginLeft = barrMarginLeft + 2000;
         }
 
         if (i>=5){
-            barrMarginLeft = barrMarginLeft + 250;
+            barrMarginLeft = barrMarginLeft + 1000;
         }
 
     }
 }
+
+var barrAnimationId = 0;
+ function barrAnimation(){
+     for (var i =0; i<10; i++){
+         var barr = document.getElementById('barr'+i);
+         var currentMarginLeft = getComputedStyle(barr).marginLeft;
+         var newMargingLeft = parseInt(currentMarginLeft)-35;
+          barr.style.marginLeft = newMargingLeft+"px";
+
+         if(newMargingLeft >= -100 & newMargingLeft <= 100){
+
+             if (boyMarginTop > 300){
+                 clearInterval(barrAnimationId);
+
+                 clearInterval(runAimatioNumber);
+                 runAimatioNumber = -1;
+
+                 clearInterval(jumpAnimNum);
+                 jumpAnimNum = -1;
+
+                 clearInterval(moveBackgrdAniId);
+                 moveBackgrdAniId = -1;
+
+                deadAnimNum =  setInterval(boyDeathAnim,100);
+             }
+         }
+     }
+ }
+
+ /*DEATH boy*/
+
+let deadImageNum = 1;
+let deadAnimNum = 0;
+
+function boyDeathAnim(){
+
+    deadImageNum = deadImageNum + 1;
+
+    if (deadImageNum === 11){
+        deadImageNum = 10;
+    }
+
+    boy.src = "assets/image/Dead("+deadImageNum+").png";
+
+    var modal = document.getElementById('gameOverModal');
+    modal.style.display = 'block';
+}
+
+
+/*Game Over*/
+$('#restartbutton').click(function (){
+    location.reload();
+});
+
+/*LEVEL 2*/
+
+function level2(){
+    if (score === 10){
+        /*var modal2 = document.getElementById('gameCompleteModal');
+        modal2.style.display = 'block';*/
+        level = level + 1 ;
+        document.getElementById('level').innerHTML = level;
+        if (level === 2){
+            document.getElementById('levelTransitionModal').style.display = 'block';
+           clearInterval(barrAnimationId);
+           clearInterval(runAimatioNumber);
+            clearInterval(moveBackgrdAniId);
+
+        }
+    }
+    if (score === 20){
+        level = level + 1 ;
+        document.getElementById('level').innerHTML = level;
+        if (level === 3){
+            document.getElementById('levelTransitionModal').style.display = 'block';
+            clearInterval(barrAnimationId);
+            clearInterval(runAimatioNumber);
+            clearInterval(moveBackgrdAniId);
+
+        }
+    }
+
+    if (score === 30){
+        level = level + 1 ;
+        document.getElementById('level').innerHTML = level;
+        if (level === 4){
+            document.getElementById('gameCompleteModal1').style.display = 'block';
+            clearInterval(barrAnimationId);
+            clearInterval(runAimatioNumber);
+            clearInterval(moveBackgrdAniId);
+
+        }
+    }
+}
+
+$('#levelGo').click(function (){
+    document.getElementById('levelTransitionModal').style.display = 'none';
+    barrAnimationId = setInterval(barrAnimation,100);
+    /*runAimatioNumber = setInterval(runanimation,100);*/
+    moveBackgrdAniId = setInterval(moveBackground,100);
+});
+
+$('#restartButton1').click(function (){
+    location.reload();
+});
 
 
 
